8222b820e7431f90489188db3cc8f05babde1e89
Elijah <122788521+elijahjf@users.noreply.github.com>
2024-02-15 20:11:23 +1100

    Add Card model, and relate user and card

---
+++ models/card.py
@@ -0,0 +1,39 @@
+from init import db, ma
+from marshmallow import fields
+
+class Card(db.Model):
+    __tablename__ = "cards" #this gives name to the table cards
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    date = db.Column(db.Date) # Date the card was created (not yet on our erd)
+    status = db.Column(db.String)
+    priority = db.Column(db.String)
+    # fk 
+    user_id =db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) # id column of the users table in the erd and it is one and only one - or rather the id's class model name for __tablename__
+    # fk is from postgress above
+
+    # this below is a feature not of postgres but of sqlalchemy, which is why it is not used above
+    # feature of orm sqlalchemy which deals with model name to exact info of user directly
+    user = db.relationship('User', back_populates='cards')
+    # {id: 1, title: Card 1, user_id: 2}
+    # {
+    #   id: 1,
+    #   title: Card 1,
+    #   user: {
+    #       name: User 1,
+    #       email: user1@email.com
+    #   }
+    # }
+
+    class CardSchema(ma.Schema):
+         # this is from ma so we refer to schema
+        user = fields.Nested('UserSchema', only = ['name', 'email'])
+
+        class Meta:
+            fields = ('id', 'title', 'description', 'date', 'status', 'priority', 'user')
+
+    # we make this so later we can dump which is a part of ma to make into python obj so it can be read by flask and put data in proper way for application
+    card_schema = CardSchema()
+    cards_schema = CardSchema(many=True)
--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 from init import db, ma
+from marshmallow import fields
 
 # extending model class to create our own models here
 class User(db.Model):
@@ -10,7 +11,25 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
+    # fk
+    cards = db.relationship('Card', back_populates='user', cascade='all delete') # i think this means if user is del then del all their cards?
+
+   # {id: 1, name: User 1, email: user1@email.com}
+    # {
+    #   id: 1,
+    #   name: User 1,
+    #   email: user1@email.com,
+    #   cards: [
+    #       {id: 1, title: Card 1},
+    #       {id: 3, title: Card 3},
+    #       {id: 7, title: Card 7}
+    #   ]
+    # }
+
 class UserSchema(ma.Schema):
+
+    cards = fields.List(fields.Nested('CardSchema', exclude['user'])
+                       )
     class Meta:
         fields = ('id', 'name', 'email', 'password', 'is_admin')
 
